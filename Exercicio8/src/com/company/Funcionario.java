7edd4aea0e63f5dd8694a8099e8ad835ff4ab714
Gustavo Betete <gustavobetete@hotmail.com>
2022-02-18 10:36:10 -0300

    Iniciando desafio 8

---
+++ Exercicio8/src/com/company/Funcionario.java
@@ -0,0 +1,49 @@
+package com.company;
+
+public class Funcionario {
+    String nome;
+    double salario;
+    double bonus;
+    double salarioLiquido;
+    double desconto;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public double getSalario() {
+        return salario;
+    }
+
+    public void setSalario(double salario) {
+        this.salario = salario;
+    }
+
+    public double getBonus() {
+        return bonus;
+    }
+
+    public void setBonus(double bonus) {
+        this.bonus = bonus;
+    }
+
+    public double getSalarioLiquido() {
+        return salarioLiquido;
+    }
+
+    public void setSalarioLiquido(double salarioLiquido) {
+        this.salarioLiquido = salarioLiquido;
+    }
+
+    public double getDesconto() {
+        return desconto;
+    }
+
+    public void setDesconto(double desconto) {
+        this.desconto = desconto;
+    }
+}
---
+++ Exercicio8/src/com/company/Main.java
@@ -0,0 +1,18 @@
+package com.company;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        List<Funcionario> funcionarios = new ArrayList<>();
+
+        System.out.println("Digite a quantidade de funcionarios:");
+        int qtdFuncionarios = sc.nextInt();
+        
+    }
+}
