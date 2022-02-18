aa6c64c8ac4b8d15e46f92253225eaf3734b2a2b
Gustavo Betete <gustavobetete@hotmail.com>
2022-02-18 01:00:50 -0300

    Adicionando novas funcionalidades

--- exercicio7/src/com/company/Main.java
+++ exercicio7/src/com/company/Main.java
@@ -14,9 +14,11 @@ public class Main {
 
         List<String> loginCadastrado = new ArrayList<>();
         loginCadastrado.add("alura");
+        loginCadastrado.add("gustavo");
 
         List<String> senhaCadastrada = new ArrayList<>();
         senhaCadastrada.add("alura123");
+        senhaCadastrada.add("betete");
 
         int maximoTentativas = 3;
         int tentativaAtual = 1;
@@ -28,32 +30,40 @@ public class Main {
             System.out.println("Informe sua senha:");
             String senhaInformada = sc.next().toLowerCase();
 
-            if (loginInformado.equals(loginCadastrado.get(0)) && senhaInformada.equals(senhaCadastrada.get(0))) {
+            boolean naoLogou = true;
+            int logou = 0;
 
-                System.out.println("Bem vindo ao sistema! " + "Sr./a " + loginInformado);
-                tentativaAtual = maximoTentativas;
-                Date dataHoraAtual = new Date();
+            for (int i = 0;i < loginCadastrado.size();i++) {
+                if (loginInformado.equals(loginCadastrado.get(i)) && senhaInformada.equals(senhaCadastrada.get(i))) {
+                    naoLogou = false;
+                    logou = 1;
+                    System.out.println("Bem vindo ao sistema! " + "Sr./a " + loginInformado);
+                    tentativaAtual = maximoTentativas;
+                    Date dataHoraAtual = new Date();
 
-                String hora = new SimpleDateFormat("HH").format(dataHoraAtual);
+                    String hora = new SimpleDateFormat("HH").format(dataHoraAtual);
 
-                int numero = Integer.parseInt(hora);
-                if (numero >=6 && numero < 12) {
-                    System.out.println("Bom dia, você se logou ao nosso sistema.");
-                }else if (numero >= 12 && numero < 18){
-                    System.out.println("Boa tarde, você se logou ao nosso sistema.");
-                }else if (numero >= 18 && numero < 24){
-                    System.out.println("Boa noite, você se logou ao nosso sistema.");
-                }else if(numero >= 0 && numero < 6){
-                    System.out.println("Boa madrugada, você se logou ao nosso sistema.");
-                }
-
-            }else {
-                if (tentativaAtual == 3) {
-                    System.out.println("Número permitido de tentativas excedeu");
-                }else {
-                    System.out.println("Login invalido. Tente novamente!");
+                    int numero = Integer.parseInt(hora);
+                    if (numero >= 6 && numero < 12) {
+                        System.out.println("Bom dia, você se logou ao nosso sistema.");
+                    } else if (numero >= 12 && numero < 18) {
+                        System.out.println("Boa tarde, você se logou ao nosso sistema.");
+                    } else if (numero >= 18 && numero < 24) {
+                        System.out.println("Boa noite, você se logou ao nosso sistema.");
+                    } else if (numero >= 0 && numero < 6) {
+                        System.out.println("Boa madrugada, você se logou ao nosso sistema.");
+                    }
                 }
             }
+                if (tentativaAtual == 3 && naoLogou) {
+                    System.out.println("Número permitido de tentativas excedeu");
+                }
+                else if (logou == 0) {
+                    System.out.println("Login invalido. Tente novamente!");
+                }
+
+
+
             tentativaAtual++;
         }
 
